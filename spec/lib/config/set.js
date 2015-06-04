d1fe2f03e40edc5ccb0fe2602a2d3ce5ab3bfc97
Lorin Beer <lorin@famo.us>
2015-06-04 14:56:17 -0700

    [#96141366] updated test expectations

--- spec/lib/config/set.js
+++ spec/lib/config/set.js
@@ -17,9 +17,11 @@ tap.test(function(t) {
     t.type(set, 'function', "set should export a function");
 
     t.test('config set execution', function(t) {
+        var data = {'devmode': false};
+
         t.plan(3);
 
-        var result = set();
+        var result = set(data);
 
         t.ok(!result, "should return a falsy value");
         t.ok(storage_mock.setGlobal.called, "should call into famous sdk setGlobal");
